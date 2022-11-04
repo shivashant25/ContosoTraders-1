c12ad740aaa01b5f75e2312ae167a1919cdbe837
mithun shanbhag <mithunshanbhag@hotmail.com>
2022-11-04 13:58:43 +0530

    cosmetic resharper edits

--- src/TailwindTraders.Api.Core/Services/Implementations/OnnxImageSearchTermPredictor.cs
+++ src/TailwindTraders.Api.Core/Services/Implementations/OnnxImageSearchTermPredictor.cs
@@ -5,21 +5,22 @@ namespace TailwindTraders.Api.Core.Services.Implementations;
 
 public class OnnxImageSearchTermPredictor : IImageSearchTermPredictor
 {
-    private readonly ILogger<OnnxImageSearchTermPredictor> logger;
-    private readonly InferenceSession session;
+    private readonly ILogger<OnnxImageSearchTermPredictor> _logger;
+
+    private readonly InferenceSession _session;
 
     public OnnxImageSearchTermPredictor(IWebHostEnvironment environment, ILogger<OnnxImageSearchTermPredictor> logger)
     {
-        this.logger = logger;
+        _logger = logger;
         var filePath = Path.Combine(environment.ContentRootPath, "Models\\OnnxModels\\products.onnx");
-        session = new InferenceSession(filePath);
+        _session = new InferenceSession(filePath);
     }
 
     public Task<string> PredictSearchTermAsync(Stream imageStream, CancellationToken cancellationToken = default)
     {
         var data = ConvertImageToTensor(imageStream);
         var input = NamedOnnxValue.CreateFromTensor("data", data);
-        using var output = session.Run(new[] {input});
+        using var output = _session.Run(new[] {input});
         var prediction = output.First(i => i.Name == "classLabel").AsEnumerable<string>().First();
         return Task.FromResult(prediction);
     }
@@ -27,21 +28,19 @@ public class OnnxImageSearchTermPredictor : IImageSearchTermPredictor
     private static DenseTensor<float> ConvertImageToTensor(Stream imageStream)
     {
         var data = new DenseTensor<float>(new[] {1, 3, 224, 224});
-        using (var image = Image.Load(imageStream))
+        using var image = Image.Load(imageStream);
+        image.Mutate(ctx => ctx.Resize(new ResizeOptions
         {
-            image.Mutate(ctx => ctx.Resize(new ResizeOptions
-            {
-                Size = new Size(224, 224),
-                Mode = ResizeMode.Stretch
-            }));
-            for (var x = 0; x < image.Width; x++)
-            for (var y = 0; y < image.Height; y++)
-            {
-                var color = image.GetPixelRowSpan(y)[x];
-                data[0, 0, x, y] = color.B;
-                data[0, 1, x, y] = color.G;
-                data[0, 2, x, y] = color.R;
-            }
+            Size = new Size(224, 224),
+            Mode = ResizeMode.Stretch
+        }));
+        for (var x = 0; x < image.Width; x++)
+        for (var y = 0; y < image.Height; y++)
+        {
+            var color = image.GetPixelRowSpan(y)[x];
+            data[0, 0, x, y] = color.B;
+            data[0, 1, x, y] = color.G;
+            data[0, 2, x, y] = color.R;
         }
 
         return data;
